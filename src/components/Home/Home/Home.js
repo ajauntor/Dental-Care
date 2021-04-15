ebb9f951ea3e3ec98611841185fa79164bc2a4ce
AJ Auntor <auntorknocks@gmail.com>
2021-04-16 01:16:11 +0600

    Made Services Detail Component

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import OurServices from '../OurServices/OurServices';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <OurServices></OurServices>
         </div>
     );
 };
--- src/components/Home/OurServices/OurServices.js
+++ src/components/Home/OurServices/OurServices.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import fluoride from '../../../images/flowride.png'
+import cavity from '../../../images/cabity.png'
+import Whitening from '../../../images/whitening.png'
+
+const serviceData = [
+    {
+        name: "Fluoride Treatment",
+        img: fluoride
+    },
+    {
+        name: "Cavity Filling",
+        img: cavity
+    },
+    {
+        name: "Teeth Whitening",
+        img: Whitening
+    }
+]
 
 const OurServices = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="service-container">
+            <div className="text-center">
+                <h5 style={{color:"#5FC7C7"}}>OUR SERVICES</h5>
+                <h2>Services We Provide</h2>
+            </div>
+        </section>
     );
 };
 
