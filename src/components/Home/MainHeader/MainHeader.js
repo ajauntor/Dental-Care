33bf6487f97b3f904e164e2c90169f355b3a8cc7
AJ Auntor <auntorknocks@gmail.com>
2021-04-15 12:34:42 +0600

    Appled d-flex align-items-center

--- src/components/Home/MainHeader/MainHeader.js
+++ src/components/Home/MainHeader/MainHeader.js
@@ -3,9 +3,14 @@ import chair from '../../../images/chair.png'
 
 const MainHeader = () => {
     return (
-        <main className="row">
+        <main style={{height:"600px"}} className="row d-flex align-items-center">
             <div className="col-md-4 offset-md-1">
+                <h1>Your pathway to a bright new smile.</h1>
 
+                <p>Dentists love the effortless handling of patient relationships. Submit and track 
+                    claims, view member benefits and reach new patients through the provider directory.</p>
+
+                    <button type="button" class="btn btn-primary">GET APPOINTMENT</button>
             </div>
             <div className="col-md-6">
                <img src={chair} alt="" className="img-fluid"/>
